7eaa9aa5cb6cbecddd089b20260f538f4040d3b5
cniki2651-arch <cniki2651@gmail.com>
2026-09-06 17:12:10 -0500

    Add files via upload

---
+++ ruta_aprendizaje_1/Anulables.kt
@@ -0,0 +1,21 @@
+//variable anulable
+
+fun main() {
+    val favoriteActor = null
+    println(favoriteActor)
+}
+
+//segundo ejercicio Int anulable
+fun main() {
+    var number: Int? = 10
+    println(number)
+}
+
+//tercer ejercicio
+fun main() {
+    var number: Int? = 10
+    println(number)
+    
+    number = null
+    println(number)
+}

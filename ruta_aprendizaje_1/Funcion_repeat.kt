2eeec747f41cc6f1c8e0f1c69eef51f2f13a446e
cniki2651-arch <cniki2651@gmail.com>
2026-09-06 19:14:46 -0500

    Add files via upload

---
+++ ruta_aprendizaje_1/Funcion_repeat.kt
@@ -0,0 +1,9 @@
+//funcion repeat
+fun main() {
+    val treatFunction = trickOrTreat(false) { "$it quarters" }
+    val trickFunction = trickOrTreat(true, null)
+    repeat(4) {
+        treatFunction()
+    }
+    trickFunction()
+}
